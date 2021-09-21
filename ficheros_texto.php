18f538830eece0d3e93ba8974f47184bcf61fa9e
gimenezdaw <abdemisa@gmail.com>
2021-09-21 19:55:26 +0200

    ficheros texto form

---
+++ ficheros_texto.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Problema texto</title>
+    </head>
+    <body>
+        <form action="guardar.php" method="post">
+            ingrese nombre: <input type="text" name="nombre" id="nombre"><br>
+            Comentarios <textarea name="comentarios" cols="40" rows="10"></textarea><br>
+            <input type="submit" value="Registrar">
+        </form>
+    </body>
+</html>
---
+++ guardar.php
@@ -0,0 +1,9 @@
+<?php
+    $nombre = $_POST['nombre'];
+    $coment = $_POST['comentarios'];
+
+    //crear fichero de texto
+    // a -> crea fichero y añade al final                  r-> leer el fichero                  w-> crea fichero y lo reescribe si existe
+    $fp = fopen('datos.txt', 'a');
+    
+?>
